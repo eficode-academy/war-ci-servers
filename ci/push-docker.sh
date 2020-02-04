fa985aa8af2141de5232bf7a9f4e9cfd0e9d00cc
Sofus Albertsen <soa@praqma.com>
2020-02-04 11:20:36 +0100

    docker env

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "$docker_password" | docker login -u "$docker_username" --password-stdin
 docker push "$docker_username/micronaut-app:1.0-${GIT_COMMIT::4}" 
 docker push "$docker_username/micronaut-app:latest" &
 wait
