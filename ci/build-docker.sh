8968b1d465000bd672aada95e2f9b169944c2f41
Sofus Albertsen <soa@praqma.com>
2020-02-04 14:14:38 +0100

    jj

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::6}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
 docker build -t "${DockerRepo}micronaut-app:latest" -t "${DockerRepo}micronaut-app:1.0-$Tag" app/
--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 echo "$docker_password" | docker login --username "$docker_username" --password-stdin
 docker push "$docker_username/micronaut-app:1.0-${GIT_COMMIT::6}" 
 docker push "$docker_username/micronaut-app:latest" &
