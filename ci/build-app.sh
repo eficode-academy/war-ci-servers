400d8090c83db55036d6d1dea17340c8931697da
Sofus Albertsen <sofusalbertsen@gmail.com>
2022-03-14 14:20:43 +0100

    trying

--- ci/build-app.sh
+++ ci/build-app.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 echo executing from "$PWD"
-docker run --rm -u gradle -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:6-jdk11 ls -al
-docker run --rm -u gradle -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:6-jdk11 gradle shadowjar -p app
+docker run --rm -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:6-jdk11 ls -al
+docker run --rm -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:6-jdk11 gradle shadowjar -p app
--- ci/unit-test-app.sh
+++ ci/unit-test-app.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run --rm -u gradle -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:6-jdk11 gradle test -p app
+docker run --rm -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:6-jdk11 gradle test -p app
