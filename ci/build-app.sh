2cb2006287d13bec72ab1e754b81185e6632659d
Sofus Albertsen <sofusalbertsen@gmail.com>
2022-03-15 09:18:35 +0100

    Delete debug code

--- ci/build-app.sh
+++ ci/build-app.sh
@@ -1,4 +1,2 @@
 #! /bin/bash
-echo executing from "$PWD"
-docker run --rm -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:6-jdk11 ls -al
 docker run --rm -v "$PWD":/home/gradle/project -w /home/gradle/project gradle:6-jdk11 gradle shadowjar -p app
